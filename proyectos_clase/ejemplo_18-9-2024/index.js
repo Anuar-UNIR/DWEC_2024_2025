99f8e504b103a3734b12385176fbce57ccd98cc8
CODEONE\AnuarKhan <anuar.khan@proeducaglobal.com>
2024-09-18 22:43:21 +0200

    Ejemplo 18-9-2024

---
+++ proyectos_clase/ejemplo_18-9-2024/index.js
@@ -0,0 +1,30 @@
+let n = 1;
+let s = 1;
+
+function mostrarMensajeExterno()
+{
+    if (n % 2 == 0)
+    {
+        alert("Es Par N: " + n);
+    }
+    else
+    {
+        alert("Es Impar N: " + n);
+    }
+    n++;
+}
+
+function mostrarMensajeExterno2()
+{
+    if (n === s)
+    {
+        alert("Son realmente iguales");
+    }
+    else
+    {
+        alert("Son distintos");
+    }
+    s = "1"
+    console.log()
+    
+}
