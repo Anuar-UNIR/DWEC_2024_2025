ed90010cbde32f346c3145c90b491f9c8c0b04be
CODEONE\AnuarKhan <anuar.khan@proeducaglobal.com>
2024-10-28 16:09:20 +0100

    Tema4_Entrenamiento_2

---
+++ Tema 4/Entrenamiento_2/testAngular/src/app/components/contador/contador.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contador',
+  standalone: true,
+  imports: [],
+  templateUrl: './contador.component.html',
+  styleUrl: './contador.component.css'
+})
+export class ContadorComponent {
+
+}
