7ba2ac8c1bb26faada911470279fc97a2559215b
CODEONE\AnuarKhan <anuar.khan@proeducaglobal.com>
2024-10-29 11:47:02 +0100

    Entrenamiento_5

---
+++ Tema 4/Entrenamiento_5/testAngular/src/app/components/reloj/reloj.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reloj',
+  standalone: true,
+  imports: [],
+  templateUrl: './reloj.component.html',
+  styleUrl: './reloj.component.css'
+})
+export class RelojComponent {
+
+}
