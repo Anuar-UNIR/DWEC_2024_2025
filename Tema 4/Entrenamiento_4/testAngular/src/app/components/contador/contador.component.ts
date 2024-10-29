fe1702fa6d991e529e94512e3b271605a5ec6252
CODEONE\AnuarKhan <anuar.khan@proeducaglobal.com>
2024-10-29 11:42:24 +0100

    Entrenamiento_4

--- Tema 4/Entrenamiento_4/testAngular/src/app/components/contador/contador.component.ts
+++ Tema 4/Entrenamiento_4/testAngular/src/app/components/contador/contador.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, SimpleChange } from '@angular/core';
 
 @Component({
   selector: 'app-contador',
@@ -8,12 +8,38 @@ import { Component } from '@angular/core';
   styleUrl: './contador.component.css'
 })
 export class ContadorComponent {
+  
   numero: number;
   color: string;
 
-  constructor(){
+  constructor() {
+    console.log("Hola desde el contructor");
     this.numero = 13;
     this.color = "azul";
   }
 
-}
+
+  ngOnInit() {
+    console.log("Hola desde el principio del ngOnInit()");
+    setTimeout(() => {
+      this.numero = 23;
+      this.color = "rojo"
+      console.log("Se han realizado cambios despues de ejecutar el constructor: " + Date());
+
+      console.log("Llamamos al destroy del component");
+      this.ngOnDestroy();
+    }, 7000);
+  }
+
+
+  ngAfterViewInit() : void {
+    console.log("Hola desde ngAfterViewInit");
+
+  }
+
+  ngOnDestroy() {
+    console.log("Hola desde ngDestroy");
+  }
+  
+  }
+
