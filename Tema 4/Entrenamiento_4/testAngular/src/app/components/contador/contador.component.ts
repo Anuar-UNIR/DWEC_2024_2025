bbcdf99512b8bae96e2c05835435a9bcacb00d22
CODEONE\AnuarKhan <anuar.khan@proeducaglobal.com>
2024-10-29 10:54:28 +0100

    Tema_4_entrenamiendo 2 y 3

---
+++ Tema 4/Entrenamiento_4/testAngular/src/app/components/contador/contador.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contador',
+  standalone: true,
+  imports: [],
+  templateUrl: './contador.component.html',
+  styleUrl: './contador.component.css'
+})
+export class ContadorComponent {
+  numero: number;
+  color: string;
+
+  constructor(){
+    this.numero = 13;
+    this.color = "azul";
+  }
+
+}
---
+++ proyectos_clase/ProyectoAngularLab/src/app/components/primer-componente/primer-componente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-primer-componente',
+  standalone: true,
+  imports: [],
+  templateUrl: './primer-componente.component.html',
+  styleUrl: './primer-componente.component.css'
+})
+export class PrimerComponenteComponent {
+
+}
