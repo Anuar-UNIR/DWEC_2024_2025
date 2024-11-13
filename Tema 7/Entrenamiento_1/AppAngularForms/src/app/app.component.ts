e7ea3dd3452ae958286ed2ef4909d5484a23e271
CODEONE\AnuarKhan <anuar.khan@proeducaglobal.com>
2024-11-13 18:18:40 +0100

    lab 6

--- Tema 7/Entrenamiento_1/AppAngularForms/src/app/app.component.ts
+++ Tema 7/Entrenamiento_1/AppAngularForms/src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PrimerComponenteComponent } from "./components/primer-componente/primer-componente.component";
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PrimerComponenteComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ProyectoAngularLab';
+  title = 'AppAngularForms';
 }
--- proyectos_clase/ProyectoAngularLab/src/app/components/primer-componente/primer-componente.component.ts
+++ proyectos_clase/ProyectoAngularLab/src/app/components/primer-componente/primer-componente.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-primer-componente',
@@ -7,6 +7,44 @@ import { Component } from '@angular/core';
   templateUrl: './primer-componente.component.html',
   styleUrl: './primer-componente.component.css'
 })
-export class PrimerComponenteComponent {
+  
+export class PrimerComponenteComponent{
+
+  nombre: string;
+  apellidos: string;
+  edad: number;
+  email: string;
+  carreras: string[];
+  carreraActual: string;
+
+  constructor() {
+    this.nombre = "Fernando";
+    this.apellidos = "Alonso";
+    this.edad = 43;
+    this.email = "alo@f1.com";
+    this.carreras = ["Monaco", "Spa", "Montmelo", "San Marino"];
+    this.carreraActual = this.carreras[Math.floor(Math.random() * this.carreras.length)];
+    // console.log("Estoy en el constructor");
+  }
+
+
+
+  // ngOnChanges(): void {
+    
+  //   console.log("Estoy en el ngOnChanges");
+  // }
+
+  // ngOnInit(): void{
+  //   console.log("Estoy en el ngOnInit");
+  //   // alert("Ya he cargado");
+
+  //   setTimeout(() => {
+  //     this.ngOnDestroy();
+  //   },5000)
+  // }
+
+  // ngOnDestroy(): void{
+  //   console.log("Estoy en el ngOnDestroy");
+  // }
 
 }
