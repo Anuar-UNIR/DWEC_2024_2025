cd3838234507cec391312ebef5b98c62889787bc
CODEONE\AnuarKhan <anuar.khan@proeducaglobal.com>
2024-11-19 22:59:52 +0100

    entrenamiento 2 tema 7

---
+++ Tema 7/Entrenamiento_2/AppFormulariosModel/src/app/components/forms-model/forms-model.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-forms-model',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './forms-model.component.html',
+  styleUrl: './forms-model.component.css'
+})
+export class FormsModelComponent {
+
+  modelForm: FormGroup;
+
+  constructor() {
+    this.modelForm = new FormGroup({
+      name: new FormControl(null, []), //donde tengo el null puedo insertar valores
+      email: new FormControl(null, []),
+      edad: new FormControl(null, []),
+      dni: new FormControl(null, []),
+      password: new FormControl(null, []),
+      repitePassword: new FormControl(null, [])
+    }, []);
+  }
+
+  getDataForm() {
+    console.log(this.modelForm.value);
+    this.modelForm.reset();
+  
+  }
+
+}
