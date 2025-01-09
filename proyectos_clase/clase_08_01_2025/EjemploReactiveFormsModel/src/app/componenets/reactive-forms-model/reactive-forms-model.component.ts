793606fba447596b75838ade80c8b9a4c6b0a563
CODEONE\AnuarKhan <anuar.khan@proeducaglobal.com>
2025-01-09 20:03:54 +0100

    Validaciones en el ReactiveFormsModel

--- proyectos_clase/clase_08_01_2025/EjemploReactiveFormsModel/src/app/componenets/reactive-forms-model/reactive-forms-model.component.ts
+++ proyectos_clase/clase_08_01_2025/EjemploReactiveFormsModel/src/app/componenets/reactive-forms-model/reactive-forms-model.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Iuser } from '../../interfaces/iuser';
 
 @Component({
@@ -17,13 +17,13 @@ export class ReactiveFormsModelComponent {
 
   constructor() {
     this.modelForm = new FormGroup({
-      name: new FormControl(null, []),
-      email: new FormControl(null, []),
-      edad: new FormControl(null, []),
-      dni: new FormControl(null, []),
-      password: new FormControl(null, []),
-      repitePassword: new FormControl(null, []),
-    }, [])
+      name: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+      email: new FormControl(null, [Validators.required, Validators.pattern(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/)]),
+      edad: new FormControl(null, [Validators.required, Validators.min(0), Validators.max(120)]),
+      dni: new FormControl(null, [Validators.required, Validators.pattern(/^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKE]$/)]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(5)]),
+      repitePassword: new FormControl(null, [Validators.required, Validators.minLength(5)]),
+    }, [this.passValidator])
 
     this.arrUsers = [];
   }
@@ -33,6 +33,16 @@ export class ReactiveFormsModelComponent {
     this.arrUsers.push(user);
     console.log(this.arrUsers);
     this.modelForm.reset();
+  }
+
+  checkControl(formControlName: string, validador: string): boolean | undefined{
+    return this.modelForm.get(formControlName)?.hasError(validador) && this.modelForm.get(formControlName)?.touched
+  }
+
+  passValidator(formValue: AbstractControl): any{
+    const pass1 = formValue.get('password')?.value;
+    const pass2 = formValue.get('repitePassword')?.value;
+    return (pass1 != pass2) ? {'passwordnotmatches' : true} : null
   }
 
 }
