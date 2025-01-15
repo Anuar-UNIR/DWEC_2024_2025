c06b5da435f48d14e84375965bbb38ad6286087a
CODEONE\AnuarKhan <anuar.khan@proeducaglobal.com>
2025-01-15 21:07:39 +0100

    clase 15-1-2025 Ejemplo rutas

--- proyectos_clase/clase_08_01_2025/EjemploReactiveFormsModel/src/app/componenets/reactive-forms-model/reactive-forms-model.component.ts
+++ proyectos_clase/clase_08_01_2025/EjemploReactiveFormsModel/src/app/componenets/reactive-forms-model/reactive-forms-model.component.ts
@@ -14,6 +14,7 @@ export class ReactiveFormsModelComponent {
   arrUsers: Iuser[];
   
   modelForm: FormGroup;
+  isNewObject: boolean = true;
 
   constructor() {
     this.modelForm = new FormGroup({
@@ -26,6 +27,32 @@ export class ReactiveFormsModelComponent {
     }, [this.passValidator])
 
     this.arrUsers = [];
+  }
+
+  ngOnInit(): void{
+    let miUser: Iuser = {
+      name: 'Fernado Alonso',
+      email: 'alonso@astonf1.com',
+      edad: 43,
+      dni: '72358245T',
+      password: 'aston12345',
+      repitePassword: 'aston12345'
+    }
+    this.isNewObject = false;
+
+    //Opcion 1
+    //this.modelForm.setValue(miUser);
+
+    //Opcion 2: Puedo meter mis propios validadores al cargar, modificar los que tenia, o no cargar controles.
+    this.modelForm = new FormGroup({
+      name: new FormControl(miUser.name, [Validators.required, Validators.minLength(3)]),
+      email: new FormControl(miUser.email, [Validators.required, Validators.pattern(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/)]),
+      edad: new FormControl(miUser.edad, [Validators.required, Validators.min(0), Validators.max(120)]),
+      dni: new FormControl(miUser.dni, [Validators.required, Validators.pattern(/^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKE]$/)]),
+      password: new FormControl(miUser.password, [Validators.required, Validators.minLength(5)]),
+      repitePassword: new FormControl(miUser.repitePassword, [Validators.required, Validators.minLength(5)]),
+    }, [this.passValidator])
+
   }
 
   getDataForm() {
@@ -44,5 +71,6 @@ export class ReactiveFormsModelComponent {
     const pass2 = formValue.get('repitePassword')?.value;
     return (pass1 != pass2) ? {'passwordnotmatches' : true} : null
   }
+  
 
 }
